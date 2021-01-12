42148616e71b9f84fb2982c6cce32e523364ed91
EltonGohJH <Eltongjh@gmail.com>
2021-01-12 20:09:10 +0800

    first Heroku deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,4 @@ class User < ActiveRecord::Base
 
 
 
-  # # password validations
-  # password_format = /\A
-  #   (?=.*[A-Z]) # Must contain an uppercase character
-  #   (?=.*[a-z]) # Must contain a lowercase character
-  #   (?=.*\d) # Must contain a digit
-  #   (?=.*[[:^alnum:]]) # Must contain a symbol
-  # /x
-  # validates :password, :length => { :minimum => 6 }
-  # validates :password, format: { with: password_format, :message => 'Password must include: 1 uppercase, 1 lowercase, 1 digit and 1 special character' }
 end
